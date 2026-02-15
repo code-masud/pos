b5236836fa340975bfd474f5db4e03735b84b0d6
code-masud <code.masud@hotmail.com>
2026-02-15 22:45:50 +0600

    Update admin.py

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Customer, CustomerAddress, LoyaltyTransaction
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer
+
+    list_display = ['id', 'name', 'email', 'phone', 'loyalty_points', 'is_active']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'email', 'phone']
+    list_filter = ['loyalty_points', 'is_active']
+    list_per_page = 10
+
+@admin.register(CustomerAddress)
+class CustomerAddressAdmin(admin.ModelAdmin):
+    model = CustomerAddress
+
+    list_display = ['id', 'customer', 'address', 'city', 'is_default']
+    list_display_links = ['id', 'customer']
+    search_fields = ['address', 'city']
+    list_filter = ['is_default']
+    list_per_page = 10
+
+@admin.register(LoyaltyTransaction)
+class LoyaltyTransactionAdmin(admin.ModelAdmin):
+    model = LoyaltyTransaction
+
+    list_display = ['id', 'customer', 'points', 'reference', 'created_at']
+    list_display_links = ['id', 'customer']
+    search_fields = ['points', 'reference']
+    list_filter = ['created_at']
+    list_per_page = 10
