570fd2fce9f56a9674aeb94ce8507c4c03dcc7b1
code-masud <code.masud@hotmail.com>
2026-02-16 12:28:03 +0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -35,6 +35,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Admin site customization
-admin.site.site_header = "POS System Administration"
-admin.site.site_title = "POS Admin Portal"
-admin.site.index_title = "Welcome to POS System Admin"
+# admin.site.site_header = "POS System Administration"
+# admin.site.site_title = "POS Admin Portal"
+# admin.site.index_title = "Welcome to POS System Admin"
